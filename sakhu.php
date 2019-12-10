065821580edba1f9ce30b25dbaaae31ec3f0a0a3
Songyot2119 <58724400+Songyot2119@users.noreply.github.com>
2019-12-10 18:51:04 +0700

    Update sakhu.php

--- sakhu.php
+++ sakhu.php
@@ -23,9 +23,8 @@
     else if($message == "711"){
        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สาคู121 ว.4 ร้านสะดวกซื้อในเขตพื้นที่รับผิดชอบ เหตุการณ์ปกติ";
+        $arrayPostData['messages'][0]['text'] = "สาคู121 ว.4 ร้านสะดวกซื้อในเขตพื้นที่รับผิดชอบ เหตุการณ์ปกติ echo date("Y-m-d H:i:s")";
        
-echo date("Y-m-d H:i:s");
         replyMsg($arrayHeader,$arrayPostData);
             
     }
