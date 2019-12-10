54e827f7ea85bc3728004282f7bcd65724ec4a31
Songyot2119 <58724400+Songyot2119@users.noreply.github.com>
2019-12-10 16:21:54 +0700

    Update sakhu.php

--- sakhu.php
+++ sakhu.php
@@ -18,7 +18,7 @@
         replyMsg($arrayHeader,$arrayPostData);
     }
 #ตัวอย่าง Message Type "Sticker"
-    else if($message == "#1 | #2"){
+    else if($message == "#1"){
        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "สาคู121 ว.4 ร้านสะดวกซื้อในเขตพื้นที่รับผิดชอบ เหตุการณ์ปกติ";
