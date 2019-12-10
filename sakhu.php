28ca532b9d80e67adaaa11cc3c524aa19a73211b
Songyot2119 <58724400+Songyot2119@users.noreply.github.com>
2019-12-10 16:11:11 +0700

    Update sakhu.php

--- sakhu.php
+++ sakhu.php
@@ -34,7 +34,7 @@
     }
     #ตัวอย่าง Message Type "Image"
     else if($message == "รูปน้องแมว"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+        $image_url = "https://manager.line.biz/api/bots/@180mismv/contents/ir-4864192-2?timestamp=1575969028292";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
         $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
