22a36f55c2ec9a7bfc10c06761cec6b010360956
Songyot2119 <58724400+Songyot2119@users.noreply.github.com>
2019-12-11 14:15:53 +0700

    Update sakhu.php

--- sakhu.php
+++ sakhu.php
@@ -103,7 +103,7 @@ $strDay= date("j",strtotime($strDate));
 $strHour= date("H",strtotime($strDate));
 $strMinute= date("i",strtotime($strDate));
 $strSeconds= date("s",strtotime($strDate));
-$strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+$strMonthCut = Array("ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
 $strMonthThai=$strMonthCut[$strMonth];
 return "$strDay $strMonthThai $strYear, $strHour:$strMinute";
 }
